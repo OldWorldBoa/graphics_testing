aaf8ec4542f6dc845c20de1bc66b0548c2b20c90
oldoworldboa <oldworldboa@gmail.com>
2025-07-28 15:38:29 -0500

    Finish texture loading

--- rustgl/src/main.rs
+++ rustgl/src/main.rs
@@ -11,17 +11,16 @@ use std::mem;
 use std::os::raw::c_void;
 use std::ptr;
 use std::sync::mpsc::Receiver;
-use std::time::Instant;
 
 pub mod shader;
 use shader::{fragment_shader, shader_builder, vertex_shader};
 
-type Triangle = [i32; 3];
-type Vertex = [f32; 8];
-
 // settings
 const SCR_WIDTH: u32 = 800;
 const SCR_HEIGHT: u32 = 600;
+const VDATA_LEN: i32 = 8;
+
+static mut TEX_SHIFT: f32 = 0.0;
 
 pub fn main() {
     // glfw: initialize and configure
@@ -45,22 +44,33 @@ pub fn main() {
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
     // Vertices are 3f Position, 3f Colour, 2f Texture Coordinate
-    let vertices: Vec<Vertex> = vec![
-        [-0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
-        [0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0],
-        [0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0],
-        [-0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0],
+    let vertices: Vec<f32> = vec![
+        -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, //v
+        0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, //v
+        0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, //v
+        -0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, //v
     ];
-    let indices: Vec<Triangle> = vec![[0, 1, 2], [0, 2, 3]];
-
-    let texture = load_img();
+    let indices: Vec<u32> = vec![
+        0, 1, 2, // t1
+        0, 2, 3, // t2
+    ];
+    let vao = create_vertex_array(vertices);
+    let ebo = create_element_buffer(indices);
+    unsafe {
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as GLint);
+    }
+    let stone_wall = load_img("res/stone-wall.jpg");
+    let troll_face = load_img("res/troll-face.jpg");
 
     let shader_vcolour = shader_builder::create_shader_program(
         vertex_shader::VSHADER_CODE.as_bytes(),
         fragment_shader::FS_VERTEX.as_bytes(),
     );
 
-    let start = Instant::now();
     while !window.should_close() {
         // events
         // -----
@@ -71,31 +81,40 @@ pub fn main() {
             gl::Clear(gl::COLOR_BUFFER_BIT);
             gl::UseProgram(shader_vcolour);
 
-            let clr_dta = CString::new("colour_shift").unwrap();
-            let clr_str: *const GLchar = clr_dta.as_ptr() as *const GLchar;
-            let colour = (start.elapsed().as_secs_f32().sin() / 2f32) + 0.5;
-            let colour_shift = gl::GetUniformLocation(shader_vcolour, clr_str);
-            gl::Uniform4f(colour_shift, 0.0, colour, 1.0, 1.0);
-            gl::BindTexture(gl::TEXTURE_2D, texture);
+            gl::ActiveTexture(gl::TEXTURE0);
+            gl::BindTexture(gl::TEXTURE_2D, stone_wall);
+            gl::ActiveTexture(gl::TEXTURE1);
+            gl::BindTexture(gl::TEXTURE_2D, troll_face);
+
+            gl::Uniform1i(get_uniform_location(shader_vcolour, "texture1"), 0);
+            gl::Uniform1i(get_uniform_location(shader_vcolour, "texture2"), 1);
+            gl::Uniform1f(get_uniform_location(shader_vcolour, "tex_shift"), TEX_SHIFT);
 
             // Draw Triangles
-            for vao in vaos.iter() {
-                gl::BindVertexArray(*vao);
-                gl::DrawArrays(gl::TRIANGLES, 0, 3);
-            }
+            gl::BindVertexArray(vao);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+            gl::BindVertexArray(0);
         }
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // glfw: swap buffers and poll IO events (keys pressed/re leased, mouse moved etc.)
         // -------------------------------------------------------------------------------
         window.swap_buffers();
         glfw.poll_events();
     }
 }
 
-fn load_img() -> u32 {
-    let texture = ImageReader::open("res/stone-wall.jpg").unwrap();
+fn get_uniform_location(shader_program: u32, name: &str) -> i32 {
+    let id = CString::new(name).unwrap();
+    let casted = id.as_ptr() as *const GLchar;
+
+    unsafe { gl::GetUniformLocation(shader_program, casted) }
+}
+
+fn load_img(file_uri: &str) -> u32 {
+    let texture = ImageReader::open(file_uri).unwrap();
     let tex_dynim = texture.decode().unwrap();
-    let tex_bytes: Vec<u8> = tex_dynim.to_rgb8().into_raw();
+    let mut tex_bytes: Vec<u16> = tex_dynim.to_rgb16().into_raw();
+    tex_bytes.reverse();
 
     let mut tex_addr: u32 = 0;
 
@@ -103,20 +122,15 @@ fn load_img() -> u32 {
         gl::GenTextures(1, &mut tex_addr);
         gl::BindTexture(gl::TEXTURE_2D, tex_addr);
 
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as GLint);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as GLint);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as GLint);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as GLint);
-
         gl::TexImage2D(
             gl::TEXTURE_2D,
             0,
-            gl::RGB8 as GLint,
+            gl::RGB4 as GLint,
             tex_dynim.width().try_into().unwrap(),
             tex_dynim.height().try_into().unwrap(),
             0,
             gl::RGB,
-            gl::UNSIGNED_BYTE,
+            gl::UNSIGNED_SHORT,
             tex_bytes.as_ptr().cast(),
         );
         gl::GenerateMipmap(gl::TEXTURE_2D);
@@ -125,7 +139,7 @@ fn load_img() -> u32 {
     tex_addr
 }
 
-fn create_element_buffer(indices: Vec<Triangle>) -> u32 {
+fn create_element_buffer(indices: Vec<u32>) -> u32 {
     let mut ebo: u32 = 0;
 
     unsafe {
@@ -134,7 +148,7 @@ fn create_element_buffer(indices: Vec<Triangle>) -> u32 {
         gl::BufferData(
             gl::ELEMENT_ARRAY_BUFFER,
             (indices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-            &indices[0],
+            &indices[0] as *const u32 as *const c_void,
             gl::STATIC_DRAW,
         );
     }
@@ -142,64 +156,56 @@ fn create_element_buffer(indices: Vec<Triangle>) -> u32 {
     ebo
 }
 
-fn gen_vaos(vertices: Vec<Vertex>) -> Vec<u32> {
-    let mut vaos = vec![];
-    let (mut vao, mut vbo) = (0, 0);
-
+fn create_vertex_array(vertices: Vec<f32>) -> u32 {
+    let mut vao: u32 = 0;
+    let mut vbo: u32 = 0;
     unsafe {
         gl::GenBuffers(1, &mut vbo);
         gl::GenVertexArrays(1, &mut vao);
         gl::BindVertexArray(vao);
         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            ((VDATA_LEN as usize) * vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            &vertices[0] as *const f32 as *const c_void,
+            gl::STATIC_DRAW,
+        );
+
+        // Position
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            VDATA_LEN * mem::size_of::<GLfloat>() as GLsizei,
+            ptr::null(),
+        );
+        gl::EnableVertexAttribArray(0);
+
+        // Colour
+        gl::VertexAttribPointer(
+            1,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            VDATA_LEN * mem::size_of::<GLfloat>() as GLsizei,
+            (3 * mem::size_of::<GLfloat>()) as *const _,
+        );
+        gl::EnableVertexAttribArray(1);
+
+        // Texture Coords
+        gl::VertexAttribPointer(
+            2,
+            2,
+            gl::FLOAT,
+            gl::FALSE,
+            VDATA_LEN * mem::size_of::<GLfloat>() as GLsizei,
+            (6 * mem::size_of::<GLfloat>()) as *const _,
+        );
+        gl::EnableVertexAttribArray(2);
     }
 
-    for vertex in vertices.iter() {
-        unsafe {
-            gl::BufferData(
-                gl::ARRAY_BUFFER,
-                (vertex.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                &vertex[0] as *const f32 as *const c_void,
-                gl::STATIC_DRAW,
-            );
-
-            // Position
-            gl::VertexAttribPointer(
-                0,
-                3,
-                gl::FLOAT,
-                gl::FALSE,
-                8 * mem::size_of::<GLfloat>() as GLsizei,
-                ptr::null(),
-            );
-            gl::EnableVertexAttribArray(0);
-
-            // Colour
-            gl::VertexAttribPointer(
-                1,
-                3,
-                gl::FLOAT,
-                gl::FALSE,
-                8 * mem::size_of::<GLfloat>() as GLsizei,
-                (3 * mem::size_of::<GLfloat>()) as *const _,
-            );
-            gl::EnableVertexAttribArray(1);
-
-            // Texture Coords
-            gl::VertexAttribPointer(
-                2,
-                2,
-                gl::FLOAT,
-                gl::FALSE,
-                8 * mem::size_of::<GLfloat>() as GLsizei,
-                (6 * mem::size_of::<GLfloat>()) as *const _,
-            );
-            gl::EnableVertexAttribArray(2);
-        }
-
-        vaos.push(vao);
-    }
-
-    vaos
+    vao
 }
 
 // NOTE: not the same version as in common.rs!
@@ -214,6 +220,14 @@ fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::Windo
             glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
                 window.set_should_close(true)
             }
+            glfw::WindowEvent::Key(Key::Up, _, Action::Press, _) => unsafe {
+                TEX_SHIFT += 0.2;
+                TEX_SHIFT = TEX_SHIFT.min(1.0);
+            },
+            glfw::WindowEvent::Key(Key::Down, _, Action::Press, _) => unsafe {
+                TEX_SHIFT -= 0.2;
+                TEX_SHIFT = TEX_SHIFT.max(0.0);
+            },
             _ => {}
         }
     }
--- rustgl/src/shader/fragment_shader.rs
+++ rustgl/src/shader/fragment_shader.rs
@@ -4,10 +4,16 @@ pub const FS_VERTEX: &str = r#"#version 330 core
 
     out vec4 frag_colour;
 
-    uniform vec4 colour_shift;
-    uniform sampler2D texture_data;
+    uniform float tex_shift;
+
+    uniform sampler2D texture1;
+    uniform sampler2D texture2;
 
     void main() {
-        frag_colour = texture(texture_data, tex_coord) * vertex_colour;
+        frag_colour = mix(
+            texture(texture1, tex_coord),
+            texture(texture2, vec2(1.0 - tex_coord.x, 1.0 - tex_coord.y)),
+            tex_shift
+        ) * vertex_colour;
     }
 "#;
